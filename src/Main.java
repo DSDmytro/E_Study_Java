c6dc9d36f8221800c90038f823cf71c277e59fa2
Dmytro <Dmytro@DESKTOP-6PKFQES>
2022-05-12 00:06:18 -0700

    Lesson1-2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String []args) {
+        int one; //declaration
+        int two; //declaration
+
+        one = 2; //assignment
+        two = 3; //assignment
+
+        System.out.println("The first value = " + one);
+        System.out.println("The second value = "+ two);
+    }
+}
